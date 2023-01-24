cbc81047725ed6ba34c947cca3a7d0ca255a8f6a
Hugo Munoz <hugo.munoz@epno.com.mx>
2023-01-23 17:46:50 -0700

    Second commit to enable first colaboratos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { Input, Layout } from 'antd';
+import './App.css';
+import Header from './components/header';
+import Footer from './components/footer';
+import CSVReader from './components/csvReader'
+
+
+const { Content } = Layout;
+const { Search } = Input;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header/>
+      <Content>
+        <Search size="large"/>
+      </Content>
+      <CSVReader/>
+      <Footer/>
+    </Layout>
   );
 }
 
